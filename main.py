36071870b16900be72761a27a5f22ce481df4c96
chuma-direktor <altaichuma04@gmail.com>
2024-10-09 13:46:37 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+print(grades)
+students = {'Johnny', ' Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(students)
+
+name = {'Aaron':(5+3+3+5+4) / 5, 'Bilbo' :(2 + 2 + 2 + 3) / 4,
+'Johnny' : (4 + 5 + 5 + 2) /4, 'Khendrik' : (4 + 4 + 3) / 3,'Steve' :(5 + 5 + 5 + 4 + 5) / 5}
+print(name)
